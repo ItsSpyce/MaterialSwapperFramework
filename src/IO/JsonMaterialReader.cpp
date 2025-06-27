691514ef32f7ee3df091d4430306c001fa14dfb2
ItsSpyce <dubstepandponies95@gmail.com>
2025-06-27 13:41:21 -0400

    Slightly less wip but it doesn't crash now!

--- src/IO/JsonMaterialReader.cpp
+++ src/IO/JsonMaterialReader.cpp
@@ -13,26 +13,36 @@ void IO::JsonMaterialReader::Read(const std::string& filename) {
   // this is setting json to null and idk why
   if (auto err = glz::read_file_json(json, filename, buffer)) {
     auto cleanedError = glz::format_error(err);
+    logger::error("Error reading JSON material file: {}", cleanedError);
     throw std::runtime_error(
-        fmt::format("Failed to read JSON material file: {}", cleanedError));
+        fmt::format("Failed to read JSON material file"));
+  }
+  if (json.is_null()) {
+    throw std::runtime_error("JSON material file is null or empty.");
   }
   logger::debug("JSON: {}", buffer.c_str());
   if (json["shaderType"].get_number() == 0) {
     BGSMFile file;
-    file.version = json["version"].get_number();
-    file.diffuseMap = json["diffuseMap"].get_string();
-    file.normalMap = json["normalMap"].get_string();
-    file.smoothSpecMap = json["smoothSpecMap"].get_string();
-    file.grayscaleMap = json["grayscaleMap"].get_string();
-    file.glowMap = json["glowMap"].get_string();
-    file.wrinkleMap = json["wrinkleMap"].get_string();
-    file.specularMap = json["specularMap"].get_string();
-    file.lightingMap = json["lightingMap"].get_string();
-    file.envMap = json["envMap"].get_string();
+    file.version = json.contains("version") ? json["version"].get_number() : 0;
+    file.diffuseMap =
+        json.contains("diffuseMap") ? json["diffuseMap"].get_string() : "";
+    file.normalMap =
+        json.contains("normalMap") ? json["normalMap"].get_string() : "";
+    file.smoothSpecMap = json.contains("smoothSpecMap")
+                             ? json["smoothSpecMap"].get_string()
+                             : "";
+    file.grayscaleMap = json.contains("grayscaleMap")
+                            ? json["grayscaleMap"].get_string() : "";
+    file.glowMap = json.contains("glowMap") ? json["glowMap"].get_string() : "";
+    file.wrinkleMap = json.contains("wrinkleMap")
+                          ? json["wrinkleMap"].get_string() : "";
+    file.specularMap = json.contains("specularMap") ? json["specularMap"].get_string() : "";
+    file.lightingMap = json.contains("lightingMap") ? json["lightingMap"].get_string() : "";
+    file.envMap = json.contains("envMap") ? json["envMap"].get_string() : "";
     file_ = std::make_shared<BGSMFile>(std::move(file));
   } else {
     BGEMFile file;
-    file.version = json["version"].get_number();
+    file.version = json.contains("version") ? json["version"].get_number() : 0;
     // TODO:
     file_ = std::make_shared<BGEMFile>(std::move(file));
   }
